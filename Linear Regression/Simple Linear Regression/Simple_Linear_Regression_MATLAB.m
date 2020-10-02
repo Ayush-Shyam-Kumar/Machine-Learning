eb8291ed08a37dc6ec9ea45eaa6029114849a297
Ayush-Shyam-Kumar <65535504+Ayush-Shyam-Kumar@users.noreply.github.com>
2020-10-03 03:02:33 +0530

    Simple Linea Regression MATLAB

---
+++ Linear Regression/Simple Linear Regression/Simple_Linear_Regression_MATLAB.m
@@ -0,0 +1,25 @@
+clear all
+close all
+clc
+m=5; c =10 ; % slope and intercepts
+x= (-5:10)'; % x is a column vector
+y=m*x+c; % y is a column vector
+n=length(x); % number of data points
+% let us generate n disturbing values from normal distribution with
+% sigma=5 . That is, standard deviation =5
+noise= 5*randn(n,1); % nx1 column vector
+yd=y+noise;
+A=[x ones(n,1)];
+Alpha= inv(A'*A)*A'*yd;
+ycap=A*Alpha;
+plot(x,yd,'*') ; % plot scattered data points
+hold on
+plot(x, ycap); % plot the regression line
+xlabel('independent variable x')
+ylabel('dependent variable y')
+ev =yd-ycap; % error vector.
+% Verify that error vector is orthogonal to column vectors of A
+Check = A'*ev ;
+% print check on screen
+Format bank
+Check
